a9511f17f4322a20333de0c621fa03ea359ab1cc
Gustavo Valdes M. <gvaldesmcl@gmail.com>
2025-02-07 18:48:24 -0300

    2da entrega en proceso

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import ItemListContainerComponent from './components/ItemListContainer/ItemListContainer'
-import NavBarComponent from './components/NavBar/NavBar'
+import ItemListContainerComponent from './components/pages/ItemListContainer/ItemListContainer'
+import NavBarComponent from './components/layouts/navBar/NavBar'
+import ProductCard from './components/common/productCard/ProductCard'
+/*import FetchingData from './components/pages/fetchingData/FetchingData'
+import Users from './components/pages/Users'
+import Comments from './components/pages/Comments'
+import ItemDetail from './components/pages/itemDetail/ItemDetail'*/
 
 
 function App() {
   return (
     <>
+    
       <NavBarComponent/>
 
       <ItemListContainerComponent texto="Bienvenido a Tienda PC Tronix"/>
+    
+      {/*<FetchingData/>
+      <Users/>
+      <Comments/>
+
+        <ItemDetail/>*/}
 
     </>
       
