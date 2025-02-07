e398490f3fab6d547193439fce67bcba047e6e4a
Gustavo Valdes M. <gvaldesmcl@gmail.com>
2025-02-07 17:45:26 -0300

    avances 07022025

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import ItemListContainerComponent from './components/pages/ItemListContainer/ItemListContainer'
 import NavBarComponent from './components/common/NavBar/NavBar'
-import FetchingData from './components/pages/fetchingData/FetchingData'
+/*import FetchingData from './components/pages/fetchingData/FetchingData'
 import Users from './components/pages/Users'
 import Comments from './components/pages/Comments'
-import ItemDetail from './components/pages/itemDetail/ItemDetail'
+import ItemDetail from './components/pages/itemDetail/ItemDetail'*/
 
 
 function App() {
   return (
     <>
-    {/*
+    
       <NavBarComponent/>
 
       <ItemListContainerComponent texto="Bienvenido a Tienda PC Tronix"/>
     
-      <FetchingData/>
+      {/*<FetchingData/>
       <Users/>
-      <Comments/>*/}
+      <Comments/>
 
-        <ItemDetail/>
+        <ItemDetail/>*/}
 
     </>
       
--- src/components/products.js
+++ src/components/products.js
@@ -4,7 +4,7 @@ export const products = [
         title:"Dell Lattitude 8090",
         price:2500,
         stock:2,
-        category:"Laptops Profesionales",
+        category:"Laptops",
         description:"Intel i5 14500K, 32 Gb Memoria DDR5,1Tb SSD NVM, Monitor 15 inch 2K ",
         imageUrl:"https://todoclick.cl/6738171-large_default/latitude-7440-14-2-in1-ci7-16g-512g-w11p.jpg"
     },
@@ -13,7 +13,7 @@ export const products = [
         title:"Lenovo Thinkpad X1",
         price:2350,
         stock:10,
-        category:"Laptops Profesionales",
+        category:"Laptops",
         description:"Intel i7 12500K, 16 Gb Memoria DDR5,512Gb SSD NVM, Monitor 13 inch inch 2K ",
         imageUrl:"https://todoclick.cl/6738171-large_default/latitude-7440-14-2-in1-ci7-16g-512g-w11p.jpg"
     },
@@ -22,7 +22,7 @@ export const products = [
         title:"Lenovo Legion 7i 16",
         price:1830,
         stock:25,
-        category:"Laptops Gamers",
+        category:"Gamer",
         description:"Intel i9 14900K, 32 Gb Memoria DDR5,1Tb SSD NVM, NVIDIA® GeForce RTX™ 4060 8GB GDDR6, Monitor 15 inch 2K ",
         imageUrl:"https://todoclick.cl/6738171-large_default/latitude-7440-14-2-in1-ci7-16g-512g-w11p.jpg"
     },
@@ -31,8 +31,26 @@ export const products = [
         title:"HP Victus 15-FA1010LA",
         price:850,
         stock:50,
-        category:"Laptops Gamers",
+        category:"Gamer",
         description:"Intel Core I5-12450H, 8GB Memoria, SSD 512GB,  GeForce RTX™ 2050",
         imageUrl:"https://todoclick.cl/6738171-large_default/latitude-7440-14-2-in1-ci7-16g-512g-w11p.jpg"
+    },
+    {
+        id:"5",
+        title:"Brother DCP-T720DW",
+        price:150,
+        stock:100,
+        category:"Impresora",
+        description:"Multifuncional Color",
+        imageUrl:"https://p2-ofp.static.pub//fes/cms/2025/01/29/gxauiuwun2w2tzs9hhb1z7gx0czbwb786686.png"
+    },
+    {
+        id:"6",
+        title:"Tablet Lenovo M11",
+        price:280,
+        stock:6,
+        category:"Tablet",
+        description:"Lenovo Tab M11 - Luna Grey",
+        imageUrl:"https://p2-ofp.static.pub//fes/cms/2025/01/29/gxauiuwun2w2tzs9hhb1z7gx0czbwb786686.png"
     }
-]
+];
